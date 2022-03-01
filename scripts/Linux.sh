fbaa2ed66035416ebad97f353c104316df365912
AnimMouse <git@animmouse.com>
2022-03-02 01:21:33 +0800

    Fix permission error

--- scripts/Linux.sh
+++ scripts/Linux.sh
@@ -3,6 +3,8 @@ version=$(curl -s https://api.github.com/repos/restic/restic/releases/latest | j
 echo ::group::Downloading Restic $version for Linux
 aria2c -x 16 https://github.com/restic/restic/releases/download/$version/restic_${version}_linux_amd64.bz2
 bzip2 -d restic_${version}_linux_amd64.bz2
+chmod +x restic_${version}_linux_amd64.bz2
 mkdir Restic
 mv restic_${version}_linux_amd64.bz2 Restic/
+chmod 
 echo ::endgroup::
