a508e3e5204930c939544dcdde2af632dba49575
AnimMouse <git@animmouse.com>
2022-03-02 11:53:17 +0800

    Restic binaries has no v on version

--- scripts/Linux.sh
+++ scripts/Linux.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 version=$(curl -s https://api.github.com/repos/restic/restic/releases/latest | jq -r '.tag_name')
 echo ::group::Downloading Restic $version for Linux
-aria2c -x 16 https://github.com/restic/restic/releases/download/$version/restic_${version}_linux_amd64.bz2
-bzip2 -d restic_${version}_linux_amd64.bz2
-chmod +x restic_${version}_linux_amd64.bz2
+aria2c -x 16 https://github.com/restic/restic/releases/download/$version/restic_${version/v}_linux_amd64.bz2
+bzip2 -d restic_${version/v}_linux_amd64.bz2
 mkdir Restic
-mv restic_${version}_linux_amd64.bz2 Restic/
-chmod 
+mv restic_${version/v}_linux_amd64 Restic/restic
+chmod +x Restic/restic
 echo ::endgroup::
